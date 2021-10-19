509319542d9bfbfe6af5ace76fcd4bb959e6c5c2
Naveen Jain <nvnjn94@hotmail.com>
2021-10-19 16:12:03 +0530

    remove unwanted code

--- full-stack-vue-graphql-starter/client/src/components/Home.vue
+++ full-stack-vue-graphql-starter/client/src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-container text-xs-center v-if="getPosts">
+  <v-container text-xs-center>
     <!-- Image Slider -->
-    <v-flex xs12>
+    <!-- <v-flex xs12>
       <v-carousel v-bind="{ cycle: true }" interval="3000">
         <v-carousel-item
           v-for="post in getPosts"
@@ -11,26 +11,7 @@
           <h1 id="carousel__title">{{ post.title }}</h1>
         </v-carousel-item>
       </v-carousel>
-    </v-flex>
-
-    <!-- Apollo Query Component to perform Query -->
-    <!-- <ApolloQuery :query="getPostQuerey">
-      <template slot-scope="{result: {loading, error, data}}">
-        <div v-if="loading">Loading ...</div>
-        <div v-else-if="error">
-          Error!! - {{error.message}}
-        </div>
-        <div v-else>
-          <ui v-for="post in data.getPosts" :key="post._id">
-            <ui>{{post.title}}</ui>
-            <ui>{{post.imageUrl}}</ui>
-            <ui>{{post.likes}}</ui>
-          </ui>
-        </div>
-      </template>
-    </ApolloQuery> -->
-
-    <!-- Image Slider -->
+    </v-flex> -->
   </v-container>
 </template>
 
@@ -39,50 +20,8 @@ import { gql } from "apollo-boost";
 
 export default {
   name: "home",
-  /** Apollo Query Defination and Execution */
-  apollo: {
-    getPosts: {
-      query: gql`
-        query {
-          getPosts {
-            _id
-            title
-            imageUrl
-            description
-            likes
-          }
-        }
-      `,
-      // Result Feteching
-      result({ loading, data }) {
-        if (!loading) {
-          this.post = data.getPosts;
-        }
-      },
-      // result(args) {
-      //   // TO Check all the Relevent Option Without Destructuring
-      //   console.dir(args);
-      // }
-      // Error Handling
-      error(err) {
-        // TO Check all the Relevent Option Without Destructuring
-        console.dir(err);
-      },
-    },
-  },
   data() {
     return {
-      // getPostQuerey: gql`
-      //   query {
-      //     getPosts {
-      //       _id
-      //       title
-      //       imageUrl
-      //       description
-      //       likes
-      //     }
-      //   }
-      // `,
       post: {},
     };
   },
