22e0a7d80b738678f6e98207f98ec18c85ebf3c0
Naveen Jain <nvnjn94@hotmail.com>
2021-10-19 15:00:33 +0530

    Apollo Query Component Demo

--- full-stack-vue-graphql-starter/client/src/components/Home.vue
+++ full-stack-vue-graphql-starter/client/src/components/Home.vue
@@ -1,13 +1,21 @@
 <template>
   <v-container>
     <h1>Home</h1>
-    <div v-if="$apollo.loading">Loading</div>
-    <div v-else>
-      <ul v-for="post in getPosts" :key="post._id">
-        <li>{{ post.title }} {{ post.imageUrl }} {{ post.description }}</li>
-        <li>{{ post.likes }}</li>
-      </ul>
-    </div>
+    <ApolloQuery :query="getPostQuerey">
+      <template slot-scope="{result: {loading, error, data}}">
+        <div v-if="loading">Loading ...</div>
+        <div v-else-if="error">
+          Error!! - {{error.message}}
+        </div>
+        <div v-else>
+          <ui v-for="post in data.getPosts" :key="post._id">
+            <ui>{{post.title}}</ui>
+            <ui>{{post.imageUrl}}</ui>
+            <ui>{{post.likes}}</ui>
+          </ui>
+        </div>
+      </template>
+    </ApolloQuery>
   </v-container>
 </template>
 
@@ -16,9 +24,40 @@ import { gql } from "apollo-boost";
 
 export default {
   name: "home",
-  apollo: {
-    getPosts: {
-      query: gql`
+  /** Apollo Query Defination and Execution */
+  // apollo: {
+  //   getPosts: {
+  //     query: gql`
+  //       query {
+  //         getPosts {
+  //           _id
+  //           title
+  //           imageUrl
+  //           description
+  //           likes
+  //         }
+  //       }
+  //     `,
+  //     // Result Feteching
+  //     result({ loading, data }) {
+  //       if (!loading) {
+  //         this.post = data.getPosts;
+  //       }
+  //     },
+  //     // result(args) {
+  //     //   // TO Check all the Relevent Option Without Destructuring
+  //     //   console.dir(args);
+  //     // }
+  //     // Error Handling
+  //     error(err) {
+  //       // TO Check all the Relevent Option Without Destructuring
+  //       console.dir(err);
+  //     },
+  //   },
+  // },
+  data() {
+    return {
+      getPostQuerey: gql`
         query {
           getPosts {
             _id
@@ -29,25 +68,6 @@ export default {
           }
         }
       `,
-      // Result Feteching
-      result({ loading, data }) {
-        if (!loading) {
-          this.post = data.getPosts;
-        }
-      },
-      // result(args) {
-      //   // TO Check all the Relevent Option Without Destructuring
-      //   console.dir(args);
-      // }
-      // Error Handling
-      error(err) {
-        // TO Check all the Relevent Option Without Destructuring
-        console.dir(err);
-      },
-    },
-  },
-  data() {
-    return {
       post: {},
     };
   },
