01ce301c8d92fe73614478c9f5207e2cbfea711b
Naveen Jain <nvnjn94@hotmail.com>
2021-10-25 17:37:08 +0530

    Snackbar component with login success snackbar implementation done

--- full-stack-vue-graphql-starter/client/src/App.vue
+++ full-stack-vue-graphql-starter/client/src/App.vue
@@ -23,6 +23,28 @@
         <transition name="fade">
           <router-view />
         </transition>
+
+        <!-- Success Snackbar -->
+        <snack-bar
+          :snackbar="logInSnackbar"
+          message="You are now sign_in !!"
+          icon="mdi-check-circle"
+          :timeout="timeout"
+          color="success"
+          actionName="Close"
+          @snackbar-action="logInSnackbar = false"
+        ></snack-bar>
+
+        <!-- Error Snackbar -->
+         <snack-bar
+          :snackbar="errorSnackbar"
+          message="You are now sign_in !!"
+          :timeout="timeout"
+          color="warning"
+          actionName="SignIn"
+          @snackbar-action="errorSnackbar = false"
+        ></snack-bar>
+
       </v-container>
     </main>
   </v-app>
@@ -34,29 +56,33 @@ import { mapGetters } from "vuex";
 /** Common Component Imports */
 import VeriticalNavBar from "./components/common/VerticalNavBar.vue";
 import HorizontalNavBar from "./components/common/HorizontalNavBar.vue";
+import SnackBar from "./components/common/SnackBar.vue";
+
 export default {
   name: "app",
   components: {
     VeriticalNavBar,
     HorizontalNavBar,
+    SnackBar
   },
   data() {
     return {
       sideNav: false,
+      logInSnackbar: false,
+      errorSnackbar: false,
+      timeout: 5000,
     };
   },
   computed: {
-    ...mapGetters(['CURRENT_USER']),
+    ...mapGetters(["CURRENT_USER"]),
     horizontalNavItems() {
       let items = [
         { icon: "mdi-message", title: "Posts", link: "/posts" },
         { icon: "mdi-lock-open", title: "Sign In", link: "/sign_in" },
         { icon: "mdi-grease-pencil", title: "Sign Up", link: "/sign_up" },
       ];
-      if(this.CURRENT_USER) {
-        items = [
-        { icon: "mdi-message", title: "Posts", link: "/posts" },
-      ]
+      if (this.CURRENT_USER) {
+        items = [{ icon: "mdi-message", title: "Posts", link: "/posts" }];
       }
       return items;
     },
@@ -66,15 +92,23 @@ export default {
         { icon: "mdi-lock-open", title: "Sign In", link: "/sign_in" },
         { icon: "mdi-grease-pencil", title: "Sign Up", link: "/sign_up" },
       ];
-      if(this.CURRENT_USER) {
+      if (this.CURRENT_USER) {
         items = [
-        { icon: "mdi-message", title: "Posts", link: "/posts" },
-        { icon: "mdi-star", title: "Add Post", link: "/post/add" },
-        { icon: "mdi-account-box", title: "Profile", link: "/profile" },
-      ];
+          { icon: "mdi-message", title: "Posts", link: "/posts" },
+          { icon: "mdi-star", title: "Add Post", link: "/post/add" },
+          { icon: "mdi-account-box", title: "Profile", link: "/profile" },
+        ];
       }
       return items;
     },
+  },
+  watch: {
+    // Manage the Login Snack bar for login success message
+    CURRENT_USER(newValue, oldValue) {
+      if(oldValue !== newValue && oldValue === null) {
+        this.logInSnackbar = true;
+      }
+    }
   },
   methods: {
     toggleNavDrawer() {
@@ -83,8 +117,8 @@ export default {
     },
     handleSignoutUser() {
       /** Action to Handle the Signout User */
-      this.$store.dispatch('signOutUser');
-    }
+      this.$store.dispatch("signOutUser");
+    },
   },
 };
 </script>
@@ -107,5 +141,9 @@ export default {
 
 .text-as-center {
   text-align: center;
+}
+
+.inline-flex-display {
+  display: inline-flex;
 }
 </style>
---
+++ full-stack-vue-graphql-starter/client/src/components/common/SnackBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-snackbar
+    :color="color"
+    v-model="snackbar"
+    :timeout="timeout"
+    bottom
+    left
+  >
+    <div class="inline-flex-display">
+      <v-icon v-if="icon" class="mr-3">{{icon}}</v-icon>
+      <h3>{{message}}</h3>
+    </div>
+    <template v-slot:action="{ attrs }">
+      <v-btn text dark v-bind="attrs" @click="$emit('snackbar-action')">
+        {{actionName}}
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  name: "Snackbar",
+  props: {
+      snackbar: {
+          type: Boolean,
+          required: true,
+          default: false
+      },
+      timeout: {
+          type: Number,
+          default: 2000,
+      },
+      message: {
+          type: String,
+          required: true,
+          default: "",
+      },
+      icon: {
+          type: String,
+          default: "",
+      },
+      actionName: {
+          type: String,
+          default: "Close"
+      },
+      color: {
+          type: String,
+          default: "primary"
+      }
+  }
+};
+</script>
